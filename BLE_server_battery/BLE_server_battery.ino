b8cf4ace71ee259c30244ad6ac2d6e0a1f0ad0ea
Diego Avellaneda <avellanedad@javeriana.edu.co>
2021-06-16 15:09:43 -0500

    change in advertising time

--- BLE_server_battery/BLE_server_battery.ino
+++ BLE_server_battery/BLE_server_battery.ino
@@ -5,10 +5,14 @@
 BLEAdvertisementData advert;
 BLEAdvertising *pAdvertising;
 
+
 int i = 0;
 
 //manufacturer code (0x02E5 for Espressif)
 int man_code = 0x02E5;
+//Para irse a dormir
+#define uS_TO_S_FACTOR 1000000  //Conversion factor for micro seconds to seconds
+#define TIME_TO_SLEEP  3        //Time ESP32 will go to sleep (in seconds)
 
 //function takes String and adds manufacturer code at the beginning 
 void setManData(String c, int c_size, BLEAdvertisementData &adv, int m_code) {
@@ -27,22 +31,31 @@ void setManData(String c, int c_size, BLEAdvertisementData &adv, int m_code) {
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
-
+  print_wakeup_reason();
   BLEDevice::init("MyESP32");
   BLEServer *pServer = BLEDevice::createServer();
 
   pAdvertising = pServer->getAdvertising();
   advert.setName("ESP32-new");
+  pAdvertising->setMinInterval(0XC8);
+  pAdvertising->setMaxInterval(0x190);
   pAdvertising->setAdvertisementData(advert);
   pAdvertising->start();
 }
 
 void loop() {
 
+  //Set timer to 5 seconds
+  //esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+
+
   if(i>20){
     i=0;
+    //Go to sleep now
+    //esp_deep_sleep_start();
+  // Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) +" Seconds");
   }
-  
+  Serial.println(i);
   i++;
   String a = String(i);
   
@@ -52,6 +65,23 @@ void loop() {
   pAdvertising->stop();
   pAdvertising->setScanResponseData(scan_response);
   pAdvertising->start();
+  delay(1000);
+
  
-  delay(2000);
+  
+}
+
+//Function that prints the reason by which ESP32 has been awaken from sleep
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+  switch(wakeup_reason)
+  {
+    case 1  : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case 2  : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case 3  : Serial.println("Wakeup caused by timer"); break;
+    case 4  : Serial.println("Wakeup caused by touchpad"); break;
+    case 5  : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.println("Wakeup was not caused by deep sleep"); break;
+  }
 }
